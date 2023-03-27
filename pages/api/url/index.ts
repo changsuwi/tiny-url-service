f699f0986cd92057e018097c7e2f57049b034bf7
Vicharm <vicharm.chang@ailabs.tw>
2023-03-27 22:03:32 +0800

    feat: when user visit website by tiny url, redirect to original url

--- pages/api/url/index.ts
+++ pages/api/url/index.ts
@@ -1,9 +1,9 @@
-import { idToKey } from '@/lib/url-shortener';
+import { idToKey, keyToID } from '@/lib/url-shortener';
 import { URLModel } from '@/lib/url/url.model';
 import { URLRepository } from '@/lib/url/url.repo';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-const allowedMethods = ['POST'];
+const allowedMethods = ['GET', 'POST'];
 
 export default async function handler(
   req: NextApiRequest,
@@ -33,6 +33,23 @@ export default async function handler(
         `generate tiny url error, [url]=${originalURL} [error]=${err}`
       );
       res.status(500).send({ message: 'generate tiny url error' });
+    }
+  }
+
+  if (req.method === 'GET') {
+    try {
+      const id = keyToID(req.query.key as string);
+      if (id < 0 || id % 1 !== 0) {
+        res.status(400).send({ message: 'invalid tiny url' });
+      } else {
+        const urlModel = await urlRepository.getURL(id);
+        res.status(200).json(urlModel);
+      }
+    } catch (err) {
+      console.error(
+        `get original url error, [key] = ${req.query.key}, [err]=${err}`
+      );
+      res.status(500).send({ message: 'get original url error' });
     }
   }
 }
