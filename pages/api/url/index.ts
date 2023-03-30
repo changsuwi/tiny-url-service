9e933f471ece04f3090a84950ad0c6f1a61158c6
Vicharm <vicharm.chang@ailabs.tw>
2023-03-30 23:54:36 +0800

    refactor: next api route

--- pages/api/url/index.ts
+++ pages/api/url/index.ts
@@ -1,60 +1,46 @@
 import { idToKey, keyToID } from '@/lib/url-shortener';
 import { URLModel } from '@/lib/url/url.model';
 import { URLRepository } from '@/lib/url/url.repo';
-import type { NextApiRequest, NextApiResponse } from 'next';
-import isURL from 'validator/lib/isURL';
+import type { NextApiHandler, NextApiResponse } from 'next';
+import { apiHandler } from '@/lib/utils/api';
+import createHttpError from 'http-errors';
+import * as Yup from 'yup';
 
-const allowedMethods = ['GET', 'POST'];
+const urlRepository = new URLRepository();
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<URLModel | { message: string }>
-) {
-  if (!allowedMethods.includes(req.method || '')) {
-    return res.status(405).send({ message: 'Method not allowed.' });
+const createTinyURL: NextApiHandler = async (
+  req,
+  res: NextApiResponse<URLModel>
+) => {
+  const schema = Yup.object().shape({
+    url: Yup.string().url().required('URL is required'),
+  });
+  const { url } = schema.validateSync(req.body);
+  let urlModel = await urlRepository.postURL(url);
+  const key = idToKey(urlModel.id);
+  urlModel = await urlRepository.updateURLKey(urlModel.id, key);
+
+  res.status(200).json(urlModel);
+};
+
+const getOriginalURLByKey: NextApiHandler = async (
+  req,
+  res: NextApiResponse<URLModel>
+) => {
+  const schema = Yup.object().shape({
+    key: Yup.string().required('Key is required'),
+  });
+  const { key } = schema.validateSync(req.query);
+  const id = keyToID(key);
+  if (id < 0 || id % 1 !== 0) {
+    throw new createHttpError.BadRequest('invalid tiny url');
+  } else {
+    const urlModel = await urlRepository.getURL(id);
+    res.status(200).json(urlModel);
   }
+};
 
-  const urlRepository = new URLRepository();
-
-  if (req.method === 'POST') {
-    const originalURL = req.body.url;
-    if (
-      !isURL(originalURL, {
-        require_protocol: true,
-      })
-    ) {
-      res.status(400).send({ message: 'invalid url' });
-      return;
-    }
-
-    try {
-      let urlModel = await urlRepository.postURL(originalURL);
-      const key = idToKey(urlModel.id);
-      urlModel = await urlRepository.updateURLKey(urlModel.id, key);
-
-      res.status(200).json(urlModel);
-    } catch (err) {
-      console.error(
-        `generate tiny url error, [url]=${originalURL} [error]=${err}`
-      );
-      res.status(500).send({ message: 'generate tiny url error' });
-    }
-  }
-
-  if (req.method === 'GET') {
-    try {
-      const id = keyToID(req.query.key as string);
-      if (id < 0 || id % 1 !== 0) {
-        res.status(400).send({ message: 'invalid tiny url' });
-      } else {
-        const urlModel = await urlRepository.getURL(id);
-        res.status(200).json(urlModel);
-      }
-    } catch (err) {
-      console.error(
-        `get original url error, [key] = ${req.query.key}, [err]=${err}`
-      );
-      res.status(500).send({ message: 'get original url error' });
-    }
-  }
-}
+export default apiHandler({
+  GET: getOriginalURLByKey,
+  POST: createTinyURL,
+});
